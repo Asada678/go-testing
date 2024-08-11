d2ed305ad331871892c74a0fe540b310417022a5
Asada678 <asada.tomoya0923@gmail.com>
2024-08-11 16:11:13 +0900

    prompt() test

--- primeapp/main_test.go
+++ primeapp/main_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"io"
+	"os"
+	"testing"
+)
 
 func Test_isPrime(t *testing.T) {
 	primeTests := []struct {
@@ -8,7 +12,7 @@ func Test_isPrime(t *testing.T) {
 		testNum  int
 		expected bool
 		msg      string
-	} {
+	}{
 		{"prime", 7, true, "7 is a prime number!"},
 		{"not prime", 8, false, "8 is not a prime number because it is divisible by 2!"},
 		{"zero", 0, false, "0 is not prime, by definition!"},
@@ -25,9 +29,36 @@ func Test_isPrime(t *testing.T) {
 		if !e.expected && result {
 			t.Errorf("%s: expected false but got true", e.name)
 		}
-		
+
 		if e.msg != msg {
 			t.Errorf("%s: expected %s but got %s", e.name, e.msg, msg)
 		}
+	}
+}
+
+func Test_prompt(t *testing.T) {
+	// save a copy of os.Stdout
+	oldOut := os.Stdout
+
+	// create a read and write pipe
+	r, w, _ := os.Pipe()
+
+	// set os.Stdout to our write pipe
+	os.Stdout = w
+
+	prompt()
+
+	// close our writer
+	_ = w.Close()
+
+	// reset os.Stdout to what it was before
+	os.Stdout = oldOut
+
+	// read the output of our prompt() func from our read pipe
+	out, _ := io.ReadAll(r)
+
+	// perform our test
+	if string(out) != "-> " {
+		t.Errorf("incorrect prompt: expected -> but got %s", string(out))
 	}
 }
